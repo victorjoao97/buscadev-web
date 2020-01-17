6320a65893505ca1c9612a02d4c04ebaf186a895
João Victor Nascimento <victorjoao97@outlook.com>
2020-01-17 20:44:29 -0300

    add procfile

---
+++ build/precache-manifest.3d5c714cc4b40e7cef1db5ac90c95e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72804c331b340fac189e8fc458a8d17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2472a6ff823925fc222",
+    "url": "/static/css/main.e58611d8.chunk.css"
+  },
+  {
+    "revision": "cfe0757d3efe63b20480",
+    "url": "/static/js/2.bf4526bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bf4526bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2472a6ff823925fc222",
+    "url": "/static/js/main.565c5b6a.chunk.js"
+  },
+  {
+    "revision": "eef48575ac53f7c5eb52",
+    "url": "/static/js/runtime-main.5d39e09b.js"
+  }
+]);
